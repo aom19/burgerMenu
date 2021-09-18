172fb433fab08741a035a80c6beab40e61f2b340
aom19 <39848284+aom19@users.noreply.github.com>
2021-09-18 23:23:17 +0300

    Media queries and send emails via email.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_o99iqnn",
+        "template_16itxzm",
+        form.current,
+        "user_jubcevDArAefYzhdrnJhz"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    form.current.reset();
+  };
   return (
     <div id="contact">
       <h1>BOOK YOUR TABLE</h1>
-      <form>
-        <input type="text" placeholder="Full Name" />
-        <input type="email" placeholder="Type your E-mail" />
-        <textarea type="text" placeholder="Write Here" />
-        <input type="submit" value="Book" />
+      <form onSubmit={sendEmail} ref={form}>
+        <input type="text" placeholder="Full Name" name="name" />
+        <input type="email" placeholder="Type your E-mail" name="email" />
+        <textarea type="text" placeholder="Write Here" name="message" />
+        <input type="submit" onClick={sendEmail} />
       </form>
     </div>
   );
